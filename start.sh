4ff174a6b347df9541eb6e83e19c32fece7c2f37
Phạm Tiến Chuẩn <ptienchuan@gmail.com>
2021-04-04 09:22:21 +0700

    add script

--- start.sh
+++ start.sh
@@ -1,4 +1,32 @@
+#!/usr/bin/env sh
 echo $SOURCE_DIRECTORY
 echo $TARGET_BRANCH
 echo $USER_NAME
 echo $USER_EMAIL
+
+# abort on errors
+set -e
+
+echo "BEFORE BUILD"
+pwd
+ls -la
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd $SOURCE_DIRECTORY
+
+echo "AFTER BUILD"
+pwd
+ls -la
+
+git init
+git config user.email $USER_EMAIL
+git config user.name $USER_NAME
+git add -A
+git commit -m 'deploy'
+
+git push -f "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" master:$TARGET_BRANCH
+
+cd -
