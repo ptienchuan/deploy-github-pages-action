b459cd1ad0518b02cf29cab2d53201b052e22cb2
Phạm Tiến Chuẩn <ptienchuan@gmail.com>
2021-04-04 09:40:14 +0700

    add log

--- start.sh
+++ start.sh
@@ -1,32 +1,25 @@
 #!/usr/bin/env sh
-echo $SOURCE_DIRECTORY
-echo $TARGET_BRANCH
-echo $USER_NAME
-echo $USER_EMAIL
-
 # abort on errors
 set -e
 
-echo "BEFORE BUILD"
-pwd
-ls -la
-
 # build
+echo "::group::Build for deploy"
 npm run build
-
-# navigate into the build output directory
 cd $SOURCE_DIRECTORY
-
-echo "AFTER BUILD"
-pwd
+echo "Source code after building"
 ls -la
+echo "::endgroup::"
 
+echo "::group::Push changes"
 git init
+
 git config user.email $USER_EMAIL
 git config user.name $USER_NAME
+
 git add -A
 git commit -m 'deploy'
 
 git push -f "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" master:$TARGET_BRANCH
+echo "::endgroup::"
 
 cd -
